6903e408ca2408ca0326b30d3cd195b3c48e96cc
Tal-01 <rose.petal.thorn@tutamail.com>
2026-04-05 13:10:58 -0600

    Test

--- UPEAddons/obj/Debug/net6.0/UPEAddons.AssemblyInfo.cs
+++ UPEAddons/obj/Debug/net6.0/UPEAddons.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UPEAddons")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adec93d44a8b548d0390e86b179ffee4243fec53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a7481d1b86c4d757870c853830b00da49f35041")]
 [assembly: System.Reflection.AssemblyProductAttribute("UPEAddons")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UPEAddons")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
